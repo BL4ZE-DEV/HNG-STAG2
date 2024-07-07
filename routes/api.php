066a2ec8d23dc0e834372d3a1bb5b48a03726f47
BL4ZE-DEV <ayedunsultan1@gmail.com>
2024-07-07 20:36:21 +0100

    HNG project GOD Abeg

---
+++ routes/api.php
@@ -0,0 +1,18 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\OrganisationController;
+use App\Http\Controllers\UserController;
+
+Route::post('/auth/register', [AuthController::class, 'register']);
+Route::post('/auth/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:api')->group(function () {
+    Route::get('/api/users/{id}', [UserController::class, 'show']);
+
+    Route::post('/api/organisations', [OrganisationController::class, 'store']);
+    Route::get('/api/organisations', [OrganisationController::class, 'index']);
+    Route::get('/api/organisations/{orgId}', [OrganisationController::class, 'show']);
+    Route::post('/api/organisations/{orgId}/users', [OrganisationController::class, 'addUser']);
+});
