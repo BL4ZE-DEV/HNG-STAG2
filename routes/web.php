e9f323328a69c0c5d5de00315e39ae3b41b7c5ae
BL4ZE-DEV <ayedunsultan1@gmail.com>
2024-07-08 14:08:53 +0100

    hng correction

--- routes/web.php
+++ routes/web.php
@@ -5,15 +5,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/api/auth/register', function () {
-    return view('register');
-});
-
-Route::get('/api/auth/login', function () {
-    return view('login');
-});
-
-Route::get('/api/user/{id}',function () {
-    return view('showuser');
-});
